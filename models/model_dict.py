f651bdf20abd71510704c7e5985245d01bccbc20
Paul Mugume Okwija <okwijapaul@gmail.com>
2025-06-19 21:35:59 +0300

    Debugging further, made input size adjustable

--- models/model_dict.py
+++ models/model_dict.py
@@ -2,13 +2,13 @@ from models.segment_anything.build_sam import sam_model_registry
 from models.segment_anything_samus.build_sam_us import samus_model_registry
 from models.segment_anything_samus_autoprompt.build_samus import autosamus_model_registry
 
-def get_model(modelname="SAM", ckpt=None, opt=None):
+def get_model(modelname="SAM", input_size=256, ckpt=None, opt=None):
     if modelname == "SAM":
         model = sam_model_registry['vit_b'](checkpoint=ckpt)
     elif modelname == "SAMUS":
-        model = samus_model_registry['vit_b'](1024, checkpoint=ckpt)
+        model = samus_model_registry['vit_b'](input_size, checkpoint=ckpt)
     elif modelname == "AutoSAMUS":
-        model = autosamus_model_registry['vit_b'](1024, checkpoint=opt.load_path)
+        model = autosamus_model_registry['vit_b'](input_size, checkpoint=opt.load_path)
     else:
         raise RuntimeError("Could not find the model:", modelname)
     return model
