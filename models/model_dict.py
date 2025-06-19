612290e1931fb95aae5740e68bfa06bb9a25966e
Paul O. Mugume <okwijapaul@gmail.com>
2025-06-19 18:57:54 +0300

    Changing model_dict to take ckpt path directly

--- models/model_dict.py
+++ models/model_dict.py
@@ -2,11 +2,11 @@ from models.segment_anything.build_sam import sam_model_registry
 from models.segment_anything_samus.build_sam_us import samus_model_registry
 from models.segment_anything_samus_autoprompt.build_samus import autosamus_model_registry
 
-def get_model(modelname="SAM", args=None, opt=None):
+def get_model(modelname="SAM", ckpt=None, opt=None):
     if modelname == "SAM":
-        model = sam_model_registry['vit_b'](checkpoint=args.sam_ckpt)
+        model = sam_model_registry['vit_b'](checkpoint=ckpt)
     elif modelname == "SAMUS":
-        model = samus_model_registry['vit_b'](args=args, checkpoint=args.sam_ckpt)
+        model = samus_model_registry['vit_b'](ckpt=args, checkpoint=ckpt)
     elif modelname == "AutoSAMUS":
         model = autosamus_model_registry['vit_b'](args=args, checkpoint=opt.load_path)
     else:
