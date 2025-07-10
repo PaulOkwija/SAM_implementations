fa45dab22f96f1a8d82d46ee9102e7b0ac739891
Paul Mugume Okwija <okwijapaul@gmail.com>
2025-07-10 16:47:20 +0300

    Debugging model building

--- models/model_dict.py
+++ models/model_dict.py
@@ -1,17 +1,17 @@
-from models.segment_anything import build_sam as sm
+from models.segment_anything.build_sam import sam_model_registry as sm
 from models.segment_anything_samus.build_sam_us import samus_model_registry
 from models.segment_anything_samus_autoprompt.build_samus import autosamus_model_registry
-from models.medsam import build_medsam as mdsm
+from models.medsam.build_medsam import sam_model_registry as mdsm
 # import models.samed.sam_lora_image_encoder as lora 
 
 
 def get_model(modelname="SAM", input_size=256, ckpt=None, opt=None, lora_cpt=None):
     if modelname == "SAM":
-        model = sm.sam_model_registry['vit_b'](checkpoint=ckpt)
+        model = sm['vit_b'](checkpoint=ckpt)
     elif modelname == "SAMUS":
         model = samus_model_registry['vit_b'](input_size, checkpoint=ckpt)
     elif modelname == "MedSAM":
-        model = mdsm.sam_model_registry['vit_b'](checkpoint=ckpt)
+        model = mdsm['vit_b'](checkpoint=ckpt)
     elif modelname == "AutoSAMUS":
         model = autosamus_model_registry['vit_b'](input_size, checkpoint=opt.load_path)
     else:
